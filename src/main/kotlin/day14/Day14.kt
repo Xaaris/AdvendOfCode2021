be514a7579d0dfe88f555523ec35f6ee69c69376
johannes.berger <johannes.berger@xing.com>
2021-12-15 17:43:17 +0100

    Day 14

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -14,16 +14,39 @@ fun solve1(input: List<String>, steps: Int): Int {
 }
 
 fun solve2(input: List<String>, steps: Int): Long {
-    var template = input.first()
+    val template = input.first()
     val insertionRules = input.drop(2).map { it.split(" -> ") }.associate { it[0] to it[1] }
+
+    var pairCounter = template.windowed(2).associateWith { 1L }
     for (step in 1..steps) {
-        println(step)
-        println(template)
-        println(template.groupingBy { it }.eachCount())
-        template = template.windowed(2).joinToString("") { it[0] + insertionRules[it]!! } + template.last()
+        pairCounter = calcNextStep(pairCounter, insertionRules)
+    println(pairCounter)
     }
-    val countOfChars: Map<Char, Int> = template.groupingBy { it }.eachCount()
-    return countOfChars.maxOf { it.value } - countOfChars.minOf { it.value }
+    val charCounter = mutableMapOf<Char, Long>()
+    for ((pair, count) in pairCounter) {
+        charCounter.merge(pair.first(), count, Long::plus)
+    }
+    charCounter.merge(template.last(), 1, Long::plus)
+    println(charCounter)
+    val max = charCounter.maxOf { it.value }
+    val min = charCounter.minOf { it.value }
+
+    println("max $max")
+    println("min $min")
+    return max - min
+}
+
+private fun calcNextStep(
+    pairCounter: Map<String, Long>,
+    insertionRules: Map<String, String>
+): MutableMap<String, Long> {
+    val newPairs = mutableMapOf<String, Long>()
+    for ((pair, count) in pairCounter) {
+        val newChar = insertionRules[pair]!!
+        newPairs.merge(pair.first() + newChar, count, Long::plus)
+        newPairs.merge(newChar + pair.last(), count, Long::plus)
+    }
+    return newPairs
 }
 
 
